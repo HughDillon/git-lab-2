147115fc48290073eba5843cf4ddc043bc525127
Hugh Dillon <hd817818@ohio.edu>
2019-09-06 15:01:30 -0400

    Committing lab program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Hugh Dillon>
+ *        Date: <9/6/19>
+ * Description: This program tests our ability to upload and
+ *              pull files from git.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
